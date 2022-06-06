36acab4310ab21bf25d20b0594db923b954c1b25
Fearless09 <toheebopeyemi9@gmail.com>
2022-06-06 15:07:52 -0700

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,82 @@
+//Monday
+const monbar = document.querySelector('.monbar');
+
+monbar.addEventListener('mouseover', (e) => {
+    document.querySelector('.montag').style.opacity = 1;
+})
+
+monbar.addEventListener('mouseout', (e) => {
+    document.querySelector('.montag').style.opacity = 0;
+})
+
+
+//Tuesday
+const tuebar = document.querySelector('.tuebar');
+
+tuebar.addEventListener('mouseover', (e) => {
+    document.querySelector('.tuetag').style.opacity = 1;;
+})
+
+tuebar.addEventListener('mouseout', (e) => {
+    document.querySelector('.tuetag').style.opacity = 0;
+})
+
+
+//Wednessday
+const wedbar = document.querySelector('.wedbar');
+
+wedbar.addEventListener('mouseover', (e) => {
+    document.querySelector('.wedtag').style.opacity = 1;;
+})
+
+wedbar.addEventListener('mouseout', (e) => {
+    document.querySelector('.wedtag').style.opacity = 0;
+})
+
+
+//Thursday
+const thubar = document.querySelector('.thubar');
+
+thubar.addEventListener('mouseover', (e) => {
+    document.querySelector('.thutag').style.opacity = 1;;
+})
+
+thubar.addEventListener('mouseout', (e) => {
+    document.querySelector('.thutag').style.opacity = 0;
+})
+
+
+//Friday
+const fribar = document.querySelector('.fribar');
+
+fribar.addEventListener('mouseover', (e) => {
+    document.querySelector('.fritag').style.opacity = 1;;
+})
+
+fribar.addEventListener('mouseout', (e) => {
+    document.querySelector('.fritag').style.opacity = 0;
+})
+
+
+//Saturday
+const satbar = document.querySelector('.satbar');
+
+satbar.addEventListener('mouseover', (e) => {
+    document.querySelector('.sattag').style.opacity = 1;;
+})
+
+satbar.addEventListener('mouseout', (e) => {
+    document.querySelector('.sattag').style.opacity = 0;
+})
+
+
+//Sunday
+const sunbar = document.querySelector('.sunbar');
+
+sunbar.addEventListener('mouseover', (e) => {
+    document.querySelector('.suntag').style.opacity = 1;;
+})
+
+sunbar.addEventListener('mouseout', (e) => {
+    document.querySelector('.suntag').style.opacity = 0;
+})
